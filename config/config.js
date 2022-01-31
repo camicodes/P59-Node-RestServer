ab449f1a0745d0a262a30354e9022bdfd4df38b0
camicodes <cammila002@hotmail.com>
2022-01-30 22:01:53 -0500

    ultimo commit

--- config/config.js
+++ config/config.js
@@ -3,3 +3,17 @@
 //========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =============================================================
+// Base de datos
+// =============================================================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/coffe';
+} else {
+    urlDB = 'mongodb+srv://bree:aWnPHhE5BilxPTy4@aravis.wbqas.mongodb.net/Cafe'
+}
+
+process.env.URLDB = urlDB;
--- server.js
+++ server.js
@@ -17,13 +17,14 @@ app.use(bodyParser.json());
 app.use(require('./server/routes/usuario'));
 
 /* Conexion con BDD */
-mongoose.connect('mongodb://localhost:27017/coffe', (err, res) => {
-    if (err) {
-        throw err;
-    }
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true },
+    (err, res) => {
+        if (err) {
+            throw err;
+        }
 
-    console.log("Base de datos ON LINE!");
-});
+        console.log("Base de datos ON LINE!");
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto:', process.env.PORT);
